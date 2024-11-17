0de29248f05d95f1b92437622632f03593ca4577
Henokia <vivekkumar85260@gmail.com>
2024-11-17 14:52:02 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -169,7 +169,7 @@ autoclean = []
 
 
 # Images
-START_IMG_URL = getenv("START_IMG_URL", None)
+START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/f4edfbd83ec3150284aae.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
