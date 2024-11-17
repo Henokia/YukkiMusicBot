44c67458df78fc20cba25a06a033f88966d202df
Henokia <vivekkumar85260@gmail.com>
2024-11-17 21:39:07 +0530

    Update start.py

--- YukkiMusic/plugins/bot/start.py
+++ YukkiMusic/plugins/bot/start.py
@@ -231,9 +231,6 @@ async def start_comm(client, message: Message, _):
 async def testbot(client, message: Message, _):
     out = start_pannel(_)
     await message.react("⚡")
-    await asyncio.sleep(0.1)  # Wait for 2 seconds
-    await message.react("🕊️")
-    await message.react("🪐")
     await message.reply_sticker("CAACAgUAAxkBAAIGJmc4uIA18pKbZrwXou93tqBwDOL-AAJaEwACIK7BVdo1lpGVyOvgNgQ")
     return await message.reply_text(
         _["start_1"].format(
